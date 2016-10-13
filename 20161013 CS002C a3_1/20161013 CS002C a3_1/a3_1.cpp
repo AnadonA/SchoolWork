3f23e56b028811eee8a79e72c2e80471b38c1dca
aanadon <aanadon@student.yosemite.edu>
2016-10-13 09:18:55 -0700

    Initial Project Creation.

---
+++ 20161013 CS002C a3_1/20161013 CS002C a3_1/a3_1.cpp
@@ -0,0 +1,16 @@
+// NAME: a3_1.cpp
+// DATE: 20161013
+// INST: DHarden CS002C
+// AUTH: Anthony Anadon (c) 2106
+// DESC:
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!" << endl;
+    return 0;
+}
+
---
+++ CSCI222 L10 T01 aanadon/CSCI222 L10 aanadon/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CSCI222 L10 aanadon")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Ingredion Incorporated")>
+<Assembly: AssemblyProduct("CSCI222 L10 aanadon")>
+<Assembly: AssemblyCopyright("Copyright © Ingredion Incorporated 2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3a5c8442-01dc-4b02-b257-8076e01df5fc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
