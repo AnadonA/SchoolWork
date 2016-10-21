1e0fd3f946a87a38f5e2eec3d56e76222937747a
aanadon <aanadon@student.yosemite.edu>
2016-10-20 19:28:31 -0700

    Just before leaving class...

---
+++ 20161020 ClassWork/20161020 ClassWork/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Structure Student
+        Public stuID As String
+        Public firstName As String
+        Public lastName As String
+        Public birthDate As Date
+    End Structure
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim myStudent As Student
+        With myStudent
+            .firstName = "Anthony"
+            .lastName = "Anadon"
+            .stuID = "w0823899"
+            .birthDate = New Date(1891, 12, 4)
+        End With
+
+    End Sub
+End Class
